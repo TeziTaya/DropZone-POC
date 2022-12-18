5dd863a1850cd4e19e55dd7bd02c6e3d3db210ea
TeziTaya <tejbir1997.singh@gmail.com>
2022-12-18 10:17:20 +0530

    added onfileadd event

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,60 @@
 <template>
-    <DropZone 
-       ref="dropzone"
-        :maxFiles="Number(10000000000)"
-        url="https://localhost:44331/api/File"
-        :uploadOnDrop="false"
-        :multipleUpload="true"
-        :acceptedFiles="['image','doc','video','png']"
-        @sending="onSending"
-        :parallelUpload="3"/>
-        <br />
-        <button @click="onUpload" >Upload</button>
+  <DropZone ref="dropzone" :maxFiles="Number(10000000000)" url="https://localhost:44331/api/File" :uploadOnDrop="false"
+    :multipleUpload="true" :acceptedFiles="['image', 'doc', 'video', 'png']"  :parallelUpload="3"
+    @addedFile="onFileAdd" @sending="onSending" />
+  <br />
+  <button @click="onUpload">Upload</button>
 
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',  
+  name: 'HelloWorld',
+  data(){
+    return{
+     filePath:[]
+    }
+  },
   props: {
     msg: String
   },
-  methods:{
-    onSending:function(files,xhr,formData){
-      formData.append('folderId','23');
-      formData.append('fileId','2');
-      formData.append('moduleName','DocStorage');
+  methods: {
+    onFileAdd: function (item) {
+      this.filePath=[];
+      this.filePath.push(item);
+      console.log(item);
     },
-    onUpload(){
+    onSending: function (files, xhr, formData) {
+      formData.append('fileDetails', JSON.stringify(this.filePath));
+      formData.append('folderId', '23');
+      formData.append('fileId', '2');
+      formData.append('moduleName', 'DocStorage');
+      // var items = event.dataTransfer.items;
+      // for (var i = 0; i < items.length; i++) {
+      //   var entry = items[i].webkitGetAsEntry();
+      //   if (entry) {
+      //     vm.traverse(vm,entry);
+      //   }
+      // }
+    },
+    traverse: function (vm,entry, path) {
+      path = path || "";
+      if (entry.isFile) {
+        // Get file
+        entry.file(function (file) {
+          console.log("File:", path + file.name);
+        });
+      } else if (entry.isDirectory) {
+        // Get folder contents
+        var dirReader = entry.createReader();
+        dirReader.readEntries(function (entries) {
+          for (var i = 0; i < entries.length; i++) {
+            vm.traverse(vm,entries[i], path + entry.name + "/");
+          }
+        });
+      }
+    },
+    onUpload() {
       this.$refs.dropzone.processQueue();
     }
   }
