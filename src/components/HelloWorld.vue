75f07c6a13300a1074ee020ec0a8eb5898f89b91
TeziTaya <tejbir1997.singh@gmail.com>
2022-12-27 23:22:08 +0530

    added unique id changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,50 @@
 <template>
-  <DropZone ref="dropzone" :maxFiles="Number(10000000000)" url="https://localhost:44331/api/File" :uploadOnDrop="false"
-    :multipleUpload="true" :acceptedFiles="['image', 'doc', 'video', 'png']"  :parallelUpload="3"
-    @addedFile="onFileAdd" @sending="onSending" />
+  <DropZone ref="dropzone" :maxFiles="3" url="https://localhost:44331/api/File" :uploadOnDrop="false"
+    :multipleUpload="true" :acceptedFiles="['image', 'doc', 'zip', 'png']"  :parallelUpload="3"
+    @addedFile="onFileAdd" @sending="onSending"/>
+    
   <br />
   <button @click="onUpload">Upload</button>
 
 </template>
-
 <script>
+import Dropzone from 'dropzone';
 export default {
   name: 'HelloWorld',
   data(){
     return{
+      dropzone: null,
      filePath:[]
     }
   },
   props: {
     msg: String
   },
+  mounted() {
+    this.dropzone = new Dropzone('form.dropzone', {
+      url: 'https://localhost:44331/api/File',
+      renameFile: function(file) {
+        return file.id+'_'+file.name;
+      }
+    });
+  },
   methods: {
     onFileAdd: function (item) {
       this.filePath=[];
+      item.file.id=item.id;
       this.filePath.push(item);
+      this.dropzone.renameFile(item.file);
       console.log(item);
+    },
+    onrenameFile:function(file){
+      let newName = file.id + '_' + file.name;
+      return newName;
     },
     onSending: function (files, xhr, formData) {
       formData.append('fileDetails', JSON.stringify(this.filePath));
-      formData.append('folderId', '23');
-      formData.append('fileId', '2');
-      formData.append('moduleName', 'DocStorage');
+      // formData.append('folderId', '23');
+      // formData.append('fileId', '2');
+      // formData.append('moduleName', 'DocStorage');
       // var items = event.dataTransfer.items;
       // for (var i = 0; i < items.length; i++) {
       //   var entry = items[i].webkitGetAsEntry();
@@ -60,4 +76,8 @@ export default {
   }
 }
 </script>
-
+<style>
+.dz-default,.dz-preview{
+  display: none !important;
+}
+</style>
